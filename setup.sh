a34c9206905f3f5a655d86a02aed3e950f4db89d
takashabe <takashabe.public@gmail.com>
2016-10-17 19:24:25 +0900

    add gitconfig

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.gvimrc ~/.gvimrc
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 
 ln -s ~/dotfiles/.vim ~/.vim
 curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
