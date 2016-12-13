cab7037fcc36afc773f2fdaabc4bbbe4a1713ea2
takashabe <takashabe.public@gmail.com>
2016-12-13 23:53:04 +0900

    fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,40 @@
+# Encoding
+set -x LC_CTYPE ja_JP.UTF-8
+set -x LC_ALL ja_JP.UTF-8
+
+# alias
+alias l 'ls -alv'
+alias ll 'ls -lv'
+alias gst 'git status'
+alias gc 'git commit -v'
+
+# for not supported 256-color
+alias ssh 'TERM=xterm ssh'
+
+# general path
+set -x PATH /usr/local/bin /usr/local/sbin $PATH
+
+# tmux auto load
+if status --is-interactive; and test -z $TMUX
+  if tmux has-session > /dev/null ^ /dev/null
+    # attach tmux session with percol like tool
+    set -l sid (tmux list-sessions | grep '' | peco | cut -d: -f1)
+    command tmux attach-session -t $sid
+  else
+    command tmux new-session
+  end
+end
+
+# rbenv
+# TODO
+
+# pyenv
+# TODO
+
+# nodebrew
+# TODO
+
+# golang
+set -x GOROOT /usr/local/opt/go/libexec
+set -x GOPATH $HOME/dev
+set -x PATH $GOPATH/bin $GOROOT/bin $PATH
