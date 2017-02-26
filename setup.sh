5addfc9d029ea6902b1c1192178dc4f17d6af17a
takashabe <takashabe.public@gmail.com>
2017-02-27 00:50:12 +0900

    use vim for neovim config

--- setup.sh
+++ setup.sh
@@ -1,21 +1,13 @@
 #!/bin/sh
 
-ln -s ~/dotfiles/.zshrc ~/.zshrc
-ln -s ~/dotfiles/.vimrc ~/.vimrc
-ln -s ~/dotfiles/.gvimrc ~/.gvimrc
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/.ideavimrc ~/.ideavimrc
-
-ln -s ~/dotfiles/.vim ~/.vim
-curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
 
 mkdir ~/bin
 cp ~/dotfiles/coloredlogcat.py ~/bin
 cp ~/dotfiles/tmuxx ~/bin
 cp ~/dotfiles/diff-highlight ~/bin
-
-curl -L http://install.ohmyz.sh | sh
 
 # app config
 # TODO: improve
@@ -27,6 +19,10 @@ ln -s ~/dotfiles/.config/fish/fish_prompt.fish ~/.config/fish/functions/fish_pro
 
 # karabiner
 ln -s ~/dotfiles/.config/karabiner/karabiner.json ~/.config/karabiner/karabiner.json
+
+# neovim
+ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.config/nvim ~/.config/nvim
 
 # TODO homebrew, ghq関連の追加
 # TODO linux/macos双方で動くようにしたい
