738a329b19ebe42d495ec3d745cd480fbc58a8c9
takashabe <takashabe.public@gmail.com>
2017-08-20 22:52:58 +0900

    Add .tigrc copy and clean unused files

--- setup.sh
+++ setup.sh
@@ -3,25 +3,20 @@
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/.ideavimrc ~/.ideavimrc
+ln -s ~/dotfiles/.tigrc ~/.tigrc
 
 mkdir ~/bin
 cp ~/dotfiles/coloredlogcat.py ~/bin
 cp ~/dotfiles/tmuxx ~/bin
 cp ~/dotfiles/diff-highlight ~/bin
 
-# app config
-# TODO: improve
-cp -r ~/dotfiles/.config ~/
-
 # fish
 ln -s ~/dotfiles/.config/fish/config.fish ~/.config/fish/config.fish
-ln -s ~/dotfiles/.config/fish/fish_prompt.fish ~/.config/fish/functions/fish_prompt.fish
 
 # karabiner
-ln -s ~/dotfiles/.config/karabiner/karabiner.json ~/.config/karabiner/karabiner.json
+ln -s ~/dotfiles/.config/karabiner ~/.config/karabiner
 
 # neovim
-ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.config/nvim ~/.config/nvim
 
 # TODO homebrew, ghq関連の追加
