a9632cad7ce5cbbdb5f66369b9eb18e805edee35
takashabe <takashabe.public@gmail.com>
2017-08-25 23:46:56 +0900

    Add global .gitignore

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/.gitignore ~/.gitignore
 ln -s ~/dotfiles/.ideavimrc ~/.ideavimrc
 ln -s ~/dotfiles/.tigrc ~/.tigrc
 
