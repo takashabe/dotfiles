87f99484bb32dff69b663a05ff0b14960eaa3d2d
takashabe <abe_takashi@cyberagent.co.jp>
2018-02-13 10:54:46 +0900

    Improve setup.sh

--- setup.sh
+++ setup.sh
@@ -1,22 +1,25 @@
 #!/bin/sh
 
-ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -s ~/dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/dotfiles/.gitignore ~/.gitignore
-ln -s ~/dotfiles/.ideavimrc ~/.ideavimrc
-ln -s ~/dotfiles/.tigrc ~/.tigrc
+## .rc files for home directory
+ln -s ~/dotfiles/.tmux.conf ~/
+ln -s ~/dotfiles/.gitconfig ~/
+ln -s ~/dotfiles/.gitignore ~/
+ln -s ~/dotfiles/.ideavimrc ~/
+ln -s ~/dotfiles/.tigrc ~/
+ln -s ~/dotfiles/.agignore ~/
 
+## ~/bin
 mkdir ~/bin
 cp ~/dotfiles/coloredlogcat.py ~/bin
 cp ~/dotfiles/tmuxx ~/bin
 cp ~/dotfiles/diff-highlight ~/bin
 
-# .config
-ln -s ~/dotfiles/.config/fish/config.fish ~/.config/fish/config.fish
-ln -s ~/dotfiles/.config/karabiner ~/.config/karabiner
-ln -s ~/dotfiles/.config/nvim ~/.config/nvim
-ln -s ~/dotfiles/.config/tmux ~/.config/tmux
-ln -s ~/dotfiles/.config/peco ~/.config/peco
+# ~/.config
+ln -s ~/dotfiles/.config/fish/config.fish ~/.config/fish/
+ln -s ~/dotfiles/.config/karabiner ~/.config/
+ln -s ~/dotfiles/.config/nvim ~/.config/
+ln -s ~/dotfiles/.config/tmux ~/.config/
+ln -s ~/dotfiles/.config/peco ~/.config/
 
 # TODO homebrew, ghq関連の追加
 # TODO linux/macos双方で動くようにしたい
