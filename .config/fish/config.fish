980dd5b2efbaea402a6f9f129619ef267395964a
takashabe <takashabe.io@gmail.com>
2020-08-27 19:51:26 +0900

    fzf 40% height

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -39,6 +39,7 @@ alias rg 'rg --hidden'
 
 ## fzf
 set -x FZF_DEFAULT_COMMAND 'rg --files --hidden --glob "!.git/*"'
+set -x FZF_DEFAULT_OPTS '--height 40% --reverse --border'
 
 # curl
 alias curl-android 'curl -A "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.167 Mobile Safari/537.36"'
