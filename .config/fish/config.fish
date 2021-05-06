8fbe0e990f6b34db7cec566fab9ffd0a80f5394b
takashabe <takashabe.io@gmail.com>
2021-05-06 10:43:57 +0900

    nvm

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -105,8 +105,9 @@ if [ -d /usr/share/nvm ] > /dev/null
   bass source /usr/share/nvm/nvm.sh
   bass source /usr/share/nvm/bash_completion
   bass source /usr/share/nvm/install-nvm-exec
-else if [ -e (brew --prefix nvm)/nvm ]
+else if [ -d (brew --prefix nvm) ]
   # for homebrew nvm
+  # need https://github.com/jorgebucaran/nvm.fish
   set -x NVM_DIR $HOME/.nvm
   bass source (brew --prefix nvm)/nvm.sh
   bass source (brew --prefix nvm)/etc/bash_completion.d/nvm
