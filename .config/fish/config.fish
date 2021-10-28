54738d4add98e0730f428d8a812e4db6a92aad76
takashabe <takashabe.io@gmail.com>
2021-10-29 00:43:29 +0900

    nodebrew

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -106,15 +106,9 @@ if [ -d /usr/share/nvm ] > /dev/null
   bass source /usr/share/nvm/bash_completion
   bass source /usr/share/nvm/install-nvm-exec
   nvm use v12.18.0 &> /dev/null
-else if [ -d (brew --prefix nvm) ]
-  if status --is-interactive
-    # for homebrew nvm
-    # need https://github.com/jorgebucaran/nvm.fish
-    set -x NVM_DIR $HOME/.nvm
-    bass source (brew --prefix nvm)/nvm.sh
-    bass source (brew --prefix nvm)/etc/bash_completion.d/nvm
-    nvm use v12.18.0 &> /dev/null
-  end
+end
+if status --is-interactive; and command -v nodebrew > /dev/null
+  set -x PATH $HOME/.nodebrew/current/bin $PATH
 end
 
 # rbenv
