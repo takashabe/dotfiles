f39fb5d16c67fe2ea7e9adda28a8688a191df684
takashabe <takashabe.io@gmail.com>
2022-03-18 16:02:51 +0900

    arandr

--- .screenlayout/basic.sh
+++ .screenlayout/basic.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output HDMI-0 --off --output DP-0 --primary --mode 3440x1440 --rate 144.00 --pos 0x0 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DP-5 --off
+xrandr --output HDMI-0 --mode 2560x1440 --pos 3840x0 --rotate right --output DP-0 --primary --mode 3840x2160 --pos 0x400 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DP-5 --off
