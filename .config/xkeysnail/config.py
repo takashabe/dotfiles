f5cdb74ab14cb917ead340e62a17abb24518f7ac
takashabe <i@takashabe.dev>
2022-10-14 00:26:14 +0900

    xkeysnail

--- .config/xkeysnail/config.py
+++ .config/xkeysnail/config.py
@@ -23,6 +23,7 @@ define_keymap(lambda wm_class: wm_class not in ("Alacritty", "Code"), {
   K("Super-z"): [K("C-z"), set_mark(False)],
   K("Super-f"): K("C-f"),
   K("Super-t"): K("C-t"),
+  K("Super-r"): K("C-r"),
   K("Super-w"): K("C-w"),
   K("Super-l"): K("C-l"),
 }, "macOS-like keys")
