01658914b543ca771a00f02ae7a1a8e38f2c8727
takashabe <i@takashabe.dev>
2023-04-04 00:40:40 +0900

    bin

--- bin/ide.sh
+++ bin/ide.sh
@@ -2,6 +2,14 @@
 
 set -euo pipefail
 
-# generate IDE-like windows
-tmux split-window -v -p 25
-tmux split-window -h
+pane_count=$(tmux list-panes | wc -l)
+
+if [ $pane_count -eq 1 ]; then
+  # If there is only 1 pane, create 2 panes at the bottom.
+  tmux split-window -v -p 25
+  tmux split-window -h
+elif [ $pane_count -eq 3 ]; then
+  # If there are already 3 panes, adjust the size of the bottom panes.
+  tmux select-pane -t 1
+  tmux resize-pane -y 25
+fi
