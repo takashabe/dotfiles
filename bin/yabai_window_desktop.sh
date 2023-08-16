19e871ac65c92bff6e487bd37436573d4b22cbc7
takashabe <i@takashabe.dev>
2023-08-16 14:00:12 +0900

    yabai

--- bin/yabai_window_desktop.sh
+++ bin/yabai_window_desktop.sh
@@ -14,13 +14,12 @@ set -euo pipefail
 
 ## display 1
 yabai -m rule --add label=alacritty app="Alacritty" display=1 space=1
+yabai -m rule --add label=vscode app="Code" display=1 space=1
 
-yabai -m rule --add label=vscode app="Code" display=1 space=2
-
-yabai -m rule --add label=edge app="Microsoft Edge" space=3
-yabai -m rule --add label=obsidian app="Obsidian" space=3
+yabai -m rule --add label=edge app="Microsoft Edge" display=1 space=2
+yabai -m rule --add label=obsidian app="Obsidian" display=1 space=2
 
 ## display 2
-yabai -m rule --add label=edge app="Microsoft Edge" display=2 space=4
-yabai -m rule --add label=slack app="Slack" display=2 space=5
-yabai -m rule --add label=gather app="Gather" display=2 space=5
+yabai -m rule --add label=edge app="Microsoft Edge" display=2 space=3
+yabai -m rule --add label=slack app="Slack" display=2 space=4
+yabai -m rule --add label=gather app="Gather" display=2 space=4
