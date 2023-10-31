b0ae76034ab4231e1abac4d6e1fcab4598e34f40
takashabe <i@takashabe.dev>
2023-10-31 22:20:15 +0900

    yabai

--- bin/yabai_window_desktop.sh
+++ bin/yabai_window_desktop.sh
@@ -17,9 +17,9 @@ yabai -m rule --add label=alacritty app="Alacritty" display=1 space=1
 yabai -m rule --add label=vscode app="Code" display=1 space=1
 
 yabai -m rule --add label=edge app="Microsoft Edge" display=1 space=2
-yabai -m rule --add label=obsidian app="Obsidian" display=1 space=2
 
 ## display 2
-yabai -m rule --add label=edge app="Microsoft Edge" display=2 space=3
+yabai -m rule --add label=obsidian app="Obsidian" display=2 space=3
+yabai -m rule --add label=notion app="Notion" display=2 space=3
 yabai -m rule --add label=slack app="Slack" display=2 space=4
 yabai -m rule --add label=gather app="Gather" display=2 space=4
