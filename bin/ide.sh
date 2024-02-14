56fa71f46e541b9526866e13bde45f650e9e9379
takashabe <i@takashabe.dev>
2024-02-14 12:07:39 +0900

    support tmxu 3.4

--- bin/ide.sh
+++ bin/ide.sh
@@ -6,10 +6,10 @@ pane_count=$(tmux list-panes | wc -l)
 
 if [ $pane_count -eq 1 ]; then
   # If there is only 1 pane, create 2 panes at the bottom.
-  tmux split-window -v -p 30
+  tmux split-window -v -l 30%
   tmux split-window -h
 elif [ $pane_count -eq 3 ]; then
   # If there are already 3 panes, adjust the size of the bottom panes.
   tmux select-pane -t 1
-  tmux resize-pane -y 30
+  tmux resize-pane -y 30%
 fi
