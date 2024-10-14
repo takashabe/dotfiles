f3c7c4723637b347a8ff86000ad56279a06a0d20
takashabe <i@takashabe.dev>
2024-10-14 14:44:04 +0900

    chore(nvim/lsp): Add formatting keymap

--- .config/nvim/lua/plugins/config/lsp.lua
+++ .config/nvim/lua/plugins/config/lsp.lua
@@ -27,6 +27,7 @@ vim.keymap.set('n', 'gt', '<cmd>lua vim.lsp.buf.type_definition()<CR>')
 vim.keymap.set('n', 'gn', '<cmd>lua vim.lsp.buf.rename()<CR>')
 vim.keymap.set('n', 'ga', '<cmd>lua vim.lsp.buf.code_action()<CR>')
 vim.keymap.set('n', 'ge', '<cmd>lua vim.diagnostic.open_float()<CR>')
+vim.keymap.set('n', 'gf', '<cmd>lua vim.lsp.buf.formatting()<CR>')
 vim.keymap.set('n', 'g]', '<cmd>lua vim.diagnostic.goto_next()<CR>')
 vim.keymap.set('n', 'g[', '<cmd>lua vim.diagnostic.goto_prev()<CR>')
 
