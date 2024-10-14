8582541f00ee40f648d8edfb389175e032ad711b
takashabe <i@takashabe.dev>
2024-10-14 14:40:23 +0900

    feat(nvim/plug): find dotfiles with telescope

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -97,6 +97,43 @@ return {
       { '<leader>fs', function() require('telescope.builtin').extension.aerial.aerial() end, mode = 'n' },
     },
     opts = {
+      defaults = {
+        file_ignore_patterns = {
+          ".git", "node_modules", "vendor",
+        },
+        vimgrep_arguments = {
+          "rg",
+          "--follow",        -- Follow symbolic links
+          "--hidden",        -- Search for hidden files
+          "--no-heading",    -- Don't group matches by each file
+          "--with-filename", -- Print the file path with the matched lines
+          "--line-number",   -- Show line numbers
+          "--column",        -- Show column numbers
+          "--smart-case",    -- Smart case search
+          "--glob=!.git/*",
+          "--glob=!node_modules/*",
+          "--glob=!vendor/*",
+        },
+      },
+      pickers = {
+        find_files = {
+          hidden = true,
+          find_command = {
+            "rg",
+            "--files",
+            "--follow",        -- Follow symbolic links
+            "--hidden",        -- Search for hidden files
+            "--no-heading",    -- Don't group matches by each file
+            "--with-filename", -- Print the file path with the matched lines
+            "--line-number",   -- Show line numbers
+            "--column",        -- Show column numbers
+            "--smart-case",    -- Smart case search
+            "--glob=!.git/*",
+            "--glob=!node_modules/*",
+            "--glob=!vendor/*",
+          },
+        },
+      },
       function()
         require("telescope").load_extension("aerial")
       end,
