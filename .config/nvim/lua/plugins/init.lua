9d22238ea6b1ae709f661860ccdd41ccc3c0cf0e
takashabe <i@takashabe.dev>
2024-10-14 14:41:22 +0900

    feat(nvim/plug): indent guide

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -296,6 +296,32 @@ return {
       },
     },
   },
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    opts = {
+      indent = {
+        char = "│",
+        tab_char = "│",
+      },
+      scope = { show_start = false, show_end = false },
+      exclude = {
+        filetypes = {
+          "help",
+          "alpha",
+          "dashboard",
+          "neo-tree",
+          "Trouble",
+          "trouble",
+          "lazy",
+          "mason",
+          "notify",
+          "toggleterm",
+          "lazyterm",
+        },
+      },
+    },
+  },
 
   -- colorscheme
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
