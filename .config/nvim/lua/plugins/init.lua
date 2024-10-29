e661e5f3975f6a47e89e9d2c03b3ee521eb716e4
takashabe <i@takashabe.dev>
2024-10-30 00:01:48 +0900

    feat(nvim): add null-ls

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -80,6 +80,17 @@ return {
       require('plugins.config.lsp')
     end,
   },
+	{
+		'nvimtools/none-ls.nvim', -- none-ls is an active community fork of null-ls
+		opts = function(_, opts)
+			local nls = require('null-ls')
+			opts.sources = vim.list_extend(opts.sources or {}, {
+				nls.builtins.code_actions.gomodifytags,
+				nls.builtins.code_actions.impl,
+			})
+			return opts
+		end,
+	},
 
   -- telescope
   {
