ab87eba0fdad76cd94967f80ec27cfde49aa1944
takashabe <i@takashabe.dev>
2024-11-04 10:17:14 +0900

    fix(nvim): masonの手動・ファイル管理が混在しているのでファイルから一旦全部消す

--- .config/nvim/lua/plugins/config/lsp.lua
+++ .config/nvim/lua/plugins/config/lsp.lua
@@ -5,11 +5,7 @@ local mason_lspconfig = require('mason-lspconfig')
 mason.setup()
 mason_lspconfig.setup({
   ensure_installed = {
-    "lua_ls",
-    "rust_analyzer",
     -- "gopls", 手動管理にする https://github.com/williamboman/mason.nvim/issues/1421
-    "golangci_lint_ls",
-    "terraformls",
   },
 })
 mason_lspconfig.setup_handlers({
