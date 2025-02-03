e5c0d5cac0727b6128b317f9f2ab1406efb31611
takashabe <i@takashabe.dev>
2025-02-03 10:39:11 +0900

    feat(nvim): sandbox mini.align

---
+++ .config/nvim/lua/plugins/sandbox.lua
@@ -0,0 +1,15 @@
+-- お試しプラグインを置く場所. プラグインの処遇が決まったらこのファイルから移動すること
+return {
+  -- 2025/01/15 align系で良さそう
+  {
+    'echasnovski/mini.align',
+    config = function()
+      require('mini.align').setup{
+        mappings = {
+          start = '<leader>ga',
+          start_with_preview = '<leader>gA',
+        },
+      }
+    end,
+  },
+}
