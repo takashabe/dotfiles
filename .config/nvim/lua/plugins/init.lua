8d8db82da01c7648f2b3eec2d0d6a2862e1686e7
takashabe <i@takashabe.dev>
2025-03-05 15:17:12 +0900

    fix(nvim): show selectioncount in lualine

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -454,9 +454,22 @@ return {
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     opts = {
       sections = {
+        lualine_a = {'mode'},
+        lualine_b = {'branch', 'diff', 'diagnostics'},
         lualine_c = {
-          { "filename", path = 1 },
+          {'filename', path = 1},
         },
+        lualine_x = {'selectioncount', 'encoding', 'fileformat', 'filetype'},
+        lualine_y = {'progress'},
+        lualine_z = {'location'}
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {'filename'},
+        lualine_x = {'location'},
+        lualine_y = {},
+        lualine_z = {}
       },
     },
   },
