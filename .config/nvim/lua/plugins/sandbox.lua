84ffebbaf1b6c37caa2d67342bfad4ee6ebe4629
takashabe <i@takashabe.dev>
2025-03-07 16:37:37 +0900

    feat(nvim): use noice.nvim

--- .config/nvim/lua/plugins/sandbox.lua
+++ .config/nvim/lua/plugins/sandbox.lua
@@ -129,6 +129,55 @@ return {
     })
   end,
   {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+    },
+    opts = {
+      cmdline = {
+        view = "cmdline_popup",
+        opts = {
+          position = {
+            row = "60%",
+          },
+        },
+      },
+      messages = {
+        view_search = "virtualtext",
+      },
+      lsp = {
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true,
+        },
+      },
+      routes = {
+        {
+          filter = {
+            event = "msg_show",
+            kind = "",
+            find = "written",
+          },
+          opts = { skip = true },
+        },
+      },
+      presets = {
+        bottom_search = true,
+        command_palette = true,
+        long_message_to_split = true,
+        inc_rename = true,
+        lsp_doc_border = true,
+      },
+      notify = {
+        -- 通知はsnacks.nvimに任せる. pickerでとりやすいため
+        enabled = false,
+      },
+    },
+  },
+  {
     "folke/todo-comments.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
     opts = {},
