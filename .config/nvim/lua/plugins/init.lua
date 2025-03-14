6bd12b82f2d66198de8968b30549c27af0cd8c8e
takashabe <i@takashabe.dev>
2025-03-14 19:39:32 +0900

    feat(nvim): use snacks.terminal() keymaps

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -95,10 +95,12 @@ return {
         { "<leader>sw",      function() Snacks.picker.grep_word() end,             desc = "Visual selection or word", mode = { "n", "x" } },
         { "<leader>st",      function() Snacks.picker.todo_comments() end,         desc = "Todo comments" },
         -- Other
-        { "<leader>gg",      function() Snacks.lazygit() end,                      desc = "Lazygit" },
-        { "<leader>un",      function() Snacks.notifier.hide() end,                desc = "Dismiss All Notifications" },
-        { "]]",              function() Snacks.words.jump(vim.v.count1) end,       desc = "Next Reference",           mode = { "n", "t" } },
-        { "[[",              function() Snacks.words.jump(-vim.v.count1) end,      desc = "Prev Reference",           mode = { "n", "t" } },
+        { "<leader>gg", function() Snacks.lazygit() end,                 desc = "Lazygit" },
+        { "<leader>un", function() Snacks.notifier.hide() end,           desc = "Dismiss All Notifications" },
+        { "<c-/>",      function() Snacks.terminal() end,                desc = "Toggle Terminal" },
+        { "<c-_>",      function() Snacks.terminal() end,                desc = "which_key_ignore" },
+        { "]]",         function() Snacks.words.jump(vim.v.count1) end,  desc = "Next Reference", mode = { "n", "t" } },
+        { "[[",         function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev Reference", mode = { "n", "t" } },
       },
     },
     config = function()
