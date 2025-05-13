b835022ad75769154b4c308008f9be519dbd890b
takashabe <i@takashabe.dev>
2025-05-13 10:46:57 +0900

    feat(nvim): lspconfig v2

--- .config/nvim/lua/plugins/config/lsp.lua
+++ .config/nvim/lua/plugins/config/lsp.lua
@@ -2,66 +2,51 @@ local mason = require("mason")
 local lspconfig = require("lspconfig")
 local mason_lspconfig = require("mason-lspconfig")
 
-local capabilities = require("blink.cmp").get_lsp_capabilities()
-
--- 固有の設定を持つサーバ群
-local server_settings = {
-  gopls = {
-    settings = {
-      capabilities = capabilities,
-      gopls = {
-        analyses = { unusedparams = true },
-        staticcheck = false,
-        gofumpt = true,
-        usePlaceholders = true,
-        buildFlags = {
-          "-tags=integration analytics_handler delivery_handler notification_handler segment_handler candidate_segment_handler overview_handler candidate_handler tenant_handler api_handler job_delivery_periodical_setting_handler free_delivery_periodical_setting_handler batch_periodical_free_delivery_handler job_handler email_communication_handler batch_handler webhook_handler batch_periodical_job_delivery_handler",
-        },
-      },
-    },
-  },
-  sqls = {
-    on_attach = function(client, bufnr)
-      require("sqls").on_attach(client, bufnr)
-    end,
-    settings = {
-      capabilities = capabilities,
-      sqls = {
-        connections = {
-          {
-            driver = "postgresql",
-            dataSourceName = "host=127.0.0.1 port=5432 user=postgres password=password dbname=postgres sslmode=disable",
-          },
-        },
-      },
-    },
-  },
-  yamlls = {
-    settings = {
-      capabilities = capabilities,
-      yaml = {
-        schemaStore = {
-          enable = true,
-          url = "https://www.schemastore.org/api/json/catalog.json",
-        },
-        validate = true,
-        completion = true,
-      },
-    },
-  },
-}
-
--- Masonのセットアップ
 mason.setup()
 mason_lspconfig.setup({
   ensure_installed = {},
   automatic_enable = true,
-  handlers = {
-    function(server_name)
-      -- サーバー固有の設定があればそれを使用、なければデフォルト設定
-      local server = server_settings[server_name] or { capabilities = capabilities }
-      lspconfig[server_name].setup(server)
-    end,
+})
+
+-- lspconfig v2
+vim.lsp.config("gopls", {
+  settings = {
+    gopls = {
+      analyses = { unusedparams = true },
+      staticcheck = false,
+      gofumpt = true,
+      usePlaceholders = true,
+      buildFlags = {
+        "-tags=integration analytics_handler delivery_handler notification_handler segment_handler candidate_segment_handler overview_handler candidate_handler tenant_handler api_handler job_delivery_periodical_setting_handler free_delivery_periodical_setting_handler batch_periodical_free_delivery_handler job_handler email_communication_handler batch_handler webhook_handler batch_periodical_job_delivery_handler",
+      },
+    },
+  },
+})
+vim.lsp.config("sqls", {
+  on_attach = function(client, bufnr)
+    require("sqls").on_attach(client, bufnr)
+  end,
+  settings = {
+    sqls = {
+      connections = {
+        {
+          driver = "postgresql",
+          dataSourceName = "host=127.0.0.1 port=5432 user=postgres password=password dbname=postgres sslmode=disable",
+        },
+      },
+    },
+  },
+})
+vim.lsp.config("yamlls", {
+  settings = {
+    yaml = {
+      schemaStore = {
+        enable = true,
+        url = "https://www.schemastore.org/api/json/catalog.json",
+      },
+      validate = true,
+      completion = true,
+    },
   },
 })
 
