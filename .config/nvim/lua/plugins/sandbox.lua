0c39fdc2c761a6138b70344a9edfcf17d1468e6b
takashabe <i@takashabe.dev>
2025-06-09 12:34:48 +0900

    feat(nvim): Add claudecode.nvim

--- .config/nvim/lua/plugins/sandbox.lua
+++ .config/nvim/lua/plugins/sandbox.lua
@@ -40,4 +40,12 @@ return {
     },
     config = true,
   },
+  {
+    "coder/claudecode.nvim",
+    config = true,
+    keys = {
+      { "<leader>ac", "<cmd>ClaudeCode<cr>", desc = "Toggle Claude" },
+      { "<leader>as", "<cmd>ClaudeCodeSend<cr>", mode = "v", desc = "Send to Claude" },
+    },
+  },
 }
