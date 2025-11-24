2db179959eae79262482a98346842046b40da88c
takashabe <i@takashabe.dev>
2025-11-25 01:09:34 +0900

    feat(fish): add fisher install script

---
+++ fisher-install.fish
@@ -0,0 +1,16 @@
+#!/usr/bin/env fish
+
+set -euo pipefail
+
+fisher install jorgebucaran/fisher
+fisher install jethrokuan/z
+fisher install edc/bass
+fisher install takashabe/fish-fzf
+fisher install takashabe/fish-pretty-theme
+fisher install z11i/github-copilot-cli.fish
+fisher install lgathy/google-cloud-sdk-fish-completion
+fisher install derekstavis/fish-neovim
+fisher install catppuccin/fish
+fisher install takashabe/fish-fzf
+fisher install takashabe/fish-pretty-theme
+fisher install takashabe/fish-git-util
